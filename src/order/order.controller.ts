0366500b5b99ab785d5383e385b483eab66b080c
Heru Anggara <heruanggara0909@gmail.com>
2023-11-02 20:38:22 +0700

    add fungsi detail cart

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { AddCartDto } from './dto/addCart.dto';
@@ -12,5 +12,12 @@ export class OrderController {
     async addToCart(@Req() req, @Body() data: AddCartDto) {
         const {id} = req.user;
         return await this.order.addToCart(id, data);
+    }
+
+    @Get('cart')
+    @UseGuards(AuthGuard)
+    async detailCart(@Req() req) {
+        const {id} = req.user
+        return await this.order.detailCart(id);
     }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -7,6 +7,12 @@ import { AddCartDto } from './dto/addCart.dto';
 export class OrderService {
     constructor(private prisma: PrismaService) {}
 
+    /**
+     * Add to cart
+     * @param id 
+     * @param data 
+     * @returns 
+     */
     async addToCart(id: string, data: AddCartDto){
         try {
             const user = await this.prisma.users.findFirst({
@@ -33,6 +39,50 @@ export class OrderService {
             return {
                 statusCode: HttpStatus.BAD_REQUEST,
                 message: 'Gagal menambhkan ke keranjang'
+            }
+        }
+    }
+
+    async detailCart(id: string) {
+        try {
+            const user = await this.prisma.users.findFirst({
+                where: { akun_id: id }
+            })
+
+            if(!user) throw new HttpException('Pengguna tidak ditemukan', HttpStatus.NOT_FOUND);
+
+            const data = await this.prisma.keranjang.findMany({
+                where: {
+                    users_id: user.id
+                },
+                select: {
+                    id: true,
+                    jumlah: true,
+                    users: {
+                        select: {
+                            nama: true
+                        }
+                    },
+                    barang: {
+                        select: {
+                            nama_produk: true,
+                            harga: true,
+                            gambar: true
+                        }
+                    }
+                }
+            })
+
+            return {
+                statusCode: HttpStatus.OK,
+                message: 'Barang di keranjang',
+                data: data
+            }
+        } catch (error) {
+            console.log(error.message);
+            return {
+                statusCode: HttpStatus.BAD_REQUEST,
+                message: 'Gagal mendapatkan data keranjang'
             }
         }
     }
